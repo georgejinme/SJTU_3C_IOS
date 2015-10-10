c16e61d39630a50fc294fad436a580b349fc7ac4
gougoumemeda <a516550257@qq.com>
2015-10-10 08:19:16 +0800

    control端tabbar初始化完成

--- 3c_control/3c_control/GestureControlController.swift
+++ 3c_control/3c_control/GestureControlController.swift
@@ -1,25 +1,22 @@
 //
-//  ViewController.swift
+//  GestureControlController.swift
 //  3c_control
 //
 //  Created by 钩钩么么哒 on 15/10/10.
 //  Copyright © 2015年 钩钩么么哒. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UITabBarController {
-
+class GestureControlController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
